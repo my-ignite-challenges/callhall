0e16b2b8ddcfd35bc83a0602adda86db7abb77c1
George W F Santos <gweverton21@hotmail.com>
2023-06-12 13:24:21 -0300

    build home layout

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,35 @@
+import { Heading, Text } from "@ignite-ui/react";
+import Image from "next/image";
+
+import previewImage from "../../assets/app-preview.png";
+
+import { Container, Hero, Preview } from "./styles";
+import { ClaimUserNameForm } from "./components/ClaimUserNameForm";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUserNameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário de agendamento"
+        />
+      </Preview>
+    </Container>
+  );
+}
